ba0e38f86c4bc601d12555eb6854f79ccaa20ee4
Josiah <josiah.pilon@yahoo.ca>
2021-03-11 22:22:04 +0000

    Created an input file to handle all the user input related stuff. Then refactored play.js

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('connecting...');
 connect();
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-  const setupInput = function() {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput);
-  stdin.resume();
-  return stdin;
-};
 setupInput();
 
 
