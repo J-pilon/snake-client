f25acab9f44751cf7f09fca1cbf1616c777d5f3f
Josiah <josiah.pilon@yahoo.ca>
2021-03-12 00:00:55 +0000

    Added funny messages

--- input.js
+++ input.js
@@ -13,6 +13,12 @@ const handleUserInput = function(key) {
   if (key === 's') {
     connection.write('Move: down');
   }
+  if (key === 'y') {
+    connection.write('Say: yolo');
+  }
+  if (key === 'f') {
+    connection.write('Say: funny');
+  }
   if (key === '\u0003') {
     process.exit();
   }
